e59a4160c714865d916315417b184b1999cfe794
bjxpolarbear <bjxpolarbear@gmail.com>
2016-07-26 19:58:10 -0400

    manual roll back3

--- poochonadmin/settings.py
+++ poochonadmin/settings.py
@@ -16,21 +16,13 @@ import urllib
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PROJECT_DIR = ''
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-
+PROJECT_DIR = ''
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-
-#
-# # Extra places for collectstatic to find static files.
-# STATICFILES_DIRS = (
-#     os.path.join(PROJECT_ROOT, 'static'),
-# )
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # login required URL
 LOGIN_URL = 'users:login'
