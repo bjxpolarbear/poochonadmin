c6786b12e7546cf32f2f32a59de2574e878df498
bjxpolarbear <bjxpolarbear@gmail.com>
2016-07-26 21:52:08 -0400

    add treebeard admin module

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
+from treebeard.admin import TreeAdmin
+from treebeard.forms import movenodeform_factory
+
+from .models import Job, JobProcedure, JobType, Procedure
 
 # Register your models here.
-from .models import Job, JobProcedure, JobType, Procedure
+
 
 admin.site.register(Job)
 admin.site.register(JobType)
-admin.site.register(Procedure)
+# admin.site.register(Procedure)
 admin.site.register(JobProcedure)
+
+
+class ProcedureAdmin(TreeAdmin):
+    form = movenodeform_factory(Procedure)
+
+admin.site.register(Procedure, ProcedureAdmin)
--- poochonadmin/settings.py
+++ poochonadmin/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'treebeard',
 
     'users.apps.UsersConfig',
     'home.apps.HomeConfig',
